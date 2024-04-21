b7fecbb280d41d77f68fcc25a4d00d50b1d409d9
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-04-21 17:26:41 -0400

    Add role & location filter checkbox sub-components

--- src/components/FilterComponent/FilterComponent.jsx
+++ src/components/FilterComponent/FilterComponent.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FilterComponent.scss';
 
-class FilterComponent extends React.Component {
-  render() {
-    return (
-      <div className="filter-component">
+import RoleFilter from '../MemberFilters/RoleFilter/RoleFilter';
+import LocationFilter from '../MemberFilters/LocationFilter/LocationFilter';
 
-        
-        
-      </div>
-    );
-  }
-}
+const FilterComponent = () => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  <RoleFilter onChange={(selectedRoles) => console.log(selectedRoles)} />
+
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  return (
+    <div className={`filter-component ${isCollapsed ? 'collapsed' : 'expanded'}`}>
+      <h2 onClick={toggleCollapse}>Filters</h2>
+      {!isCollapsed && (
+        <div>
+          { <RoleFilter /> }
+          { <LocationFilter /> }
+          {/* <IndustryFilter onChange={handleIndustryChange} selectedOptions={selectedIndustry} /> */}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default FilterComponent;
---
+++ src/components/MemberFilters/RoleFilter/RoleFilter.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const RoleFilter = ({ onChange }) => {
+  const [selectedRoles, setSelectedRoles] = useState([]);
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const roles = ['UX Designer', 'Software Engineer', 'Product Manager', 'Data Scientist', 'All', 'Other'];
+
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  const toggleRole = (role) => {
+    let newSelectedRoles = [];
+
+    if (role === 'All') {
+      newSelectedRoles = selectedRoles.includes('All') ? [] : [...roles];
+    } else {
+      if (selectedRoles.includes(role)) {
+        newSelectedRoles = selectedRoles.filter(selectedRole => selectedRole !== role);
+      } else {
+        newSelectedRoles = [...selectedRoles, role];
+      }
+
+      if (newSelectedRoles.length < roles.length - 1) {
+        newSelectedRoles = newSelectedRoles.filter(selectedRole => selectedRole !== 'All');
+      }
+    }
+
+    setSelectedRoles(newSelectedRoles);
+
+    onChange && onChange(newSelectedRoles);
+  };
+
+  return (
+    <div className="role-filter">
+      <div className={`filter-section ${isCollapsed ? 'collapsed' : ''}`}>
+        <h3 onClick={toggleCollapse}>Role</h3>
+        {!isCollapsed && (
+          roles.map((role, index) => (
+            <div key={index}>
+              <input
+                id={`role-${index}`}
+                type="checkbox"
+                checked={selectedRoles.includes(role)}
+                onChange={() => toggleRole(role)}
+              />
+              <label htmlFor={`role-${index}`}>{role}</label>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RoleFilter;
